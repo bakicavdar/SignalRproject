ab58be82600b52861538cdc424b7fc5a1425722c
baki <abdulbaki@dividesoft.com>
2023-12-19 21:52:45 +0300

    Add project files.

---
+++ SignalR.EntityLayer/Entities/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    internal class Contact
+    {
+        public int ContactID { get; set; }
+        public string Location { get; set; }
+        public string Phone { get; set; }
+        public string Mail { get; set; }
+    }
+}
